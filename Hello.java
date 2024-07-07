6ee3cf6c7fe01e37bc154c112302adbc631781b5
mohamad rageh <rageh205@gmail.com>
2024-07-07 16:59:35 +0300

    greeting function added

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
+    private static void greet(String name) {
+        System.out.println("Welcome " + name + "!");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        greet("Rageh");
     }
 }
