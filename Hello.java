5a0d30953397695c1653de99cdee051247e4c33d
Mohamed Mohey <mmi9433@gmail.com>
2024-07-10 16:33:30 +0300

    added greet function

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        greet("Student");
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 }
