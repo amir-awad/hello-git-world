0ac558cf07095525893f8b6cf06c4d9907c7264d
HusseinYEbrahim <husseinebrahim388@gmail.com>
2024-07-10 16:39:30 +0300

    fix the bug

--- Hello.java
+++ Hello.java
@@ -2,4 +2,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public static void greet(String name)
+    {
+        System.out.println("Hey " + name );
+    }
 }
