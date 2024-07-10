0a89867dc6697e21b4f4599502c1bb4b980b427e
iten.mohamed <itenmohamed7@gmail.com>
2024-07-10 14:45:22 +0300

    Merge feature-add-greeting into main

--- Hello.java
+++ Hello.java
@@ -2,4 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+      public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
 }
